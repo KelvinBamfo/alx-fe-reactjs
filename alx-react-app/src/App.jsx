6d7582040856f1b7015cb1ad5b0f2dae02979225
Kelvin Bamfo <kelvinappiah765@gmail.com>
2026-01-11 18:14:05 +0200

    Updated App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -8,10 +8,10 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return ( 
     <>
+    <WelcomeMessage/>
       <div>
-        <WelcomeMessage/>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
