e289e6d22ab2e16e654c60586448635af23a6b62
Kelvin Bamfo <kelvinappiah765@gmail.com>
2026-01-17 21:36:48 +0200

    Updated new files

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import ProfilePage from './components/ProfilePage';
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ProfilePage from './ProfilePage';
 
 function App() {
-  return ( 
-    <>
-      <div>
-      </div>
-    </>
-  )
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return <ProfilePage userData={userData} />;
 }
 
-export default App
+export default App;
