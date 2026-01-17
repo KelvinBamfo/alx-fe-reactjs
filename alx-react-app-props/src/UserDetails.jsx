c8a8a3efdcdfe545b018cb05eccd1190faf315c9
Kelvin Bamfo <kelvinappiah765@gmail.com>
2026-01-17 23:12:02 +0200

    Updated UserDetails file

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import UserContext from './components/UserContext';
 
 function UserDetails() {
-  const user = useContext(UserContext);
+  const user = ["useContext", "UserContext"];
 
   return (
     <div>
