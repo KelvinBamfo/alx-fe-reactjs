7ce59bf7d5cebf0ddfe3c19a89b03987d3eebebe
Kelvin Bamfo <kelvinappiah765@gmail.com>
2026-01-17 22:58:14 +0200

    Updated some files

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import UserContext from './UserContext'
+import UserContext from './components/UserContext'
 import ProfilePage from './ProfilePage'
 import UserInfo from './UserInfo'
 import UserDetails from './UserDetails'
