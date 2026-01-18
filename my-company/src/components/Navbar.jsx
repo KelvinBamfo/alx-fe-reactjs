46b1923981494f6f727d30423c5f15ef6d631f5f
Kelvin Bamfo <kelvinappiah765@gmail.com>
2026-01-18 19:05:59 +0200

    Fixed some bugs

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", backgroundColor: "hsl(0, 0%, 20%)", color: "hsl(0, 0%, 100%)" display: "block", justifyContent: "left" }}>
+    <nav style={{ padding: "10px", backgroundColor: "hsl(0, 0%, 20%)", color: "hsl(0, 0%, 100%)", display: "block", justifyContent: "left" }}>
       <Link to="/" style={{ margin: "0 10px", color: "hsl(0, 0%, 100%)" }}>Home</Link>
       <Link to="/About" style={{ margin: "0 10px", color: "hsl(0, 0%, 100%)" }}>About</Link>
       <Link to="/Services" style={{ margin: "0 10px", color: "hsl(0, 0%, 100%)" }}>Services</Link>
