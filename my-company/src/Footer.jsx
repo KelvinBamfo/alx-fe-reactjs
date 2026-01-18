0689441e145a23e6a9432c3cfc20d15923545c0d
Kelvin Bamfo <kelvinappiah765@gmail.com>
2026-01-18 20:29:33 +0200

    Update

--- my-company/src/Footer.jsx
+++ my-company/src/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer style={{ padding: "10px", background: "#eee", textAlign: "center" }}>
+    <footer style={{ padding: "10px", background: "hsl(0, 0%, 93%)", color: "hsl(0, 2%, 11%)", textAlign: "center" }}>
       <p>© 2026 My Company. All rights reserved.</p>
     </footer>
   );
