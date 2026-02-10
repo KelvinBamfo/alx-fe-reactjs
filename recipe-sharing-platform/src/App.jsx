9898e8eb38dba3f6c79afe3adb5080a430a4f7f8
Kelvin Bamfo <kelvinappiah765@gmail.com>
2026-02-10 21:21:55 +0200

    Added a recipe detail page

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
-
   return (
-      <div>
-        <HomePage />
-      </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
