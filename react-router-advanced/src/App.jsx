bb3d27ed3e61ec6b068b4736cff361123c4a5113
Kelvin Bamfo <kelvinappiah765@gmail.com>
2026-02-21 20:14:41 +0200

    Created an advanced Routing in React with React Router

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import BlogPost from "./components/BlogPost";
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* Protected Profile Route */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Dynamic Blog Route */}
+        <Route path="/blog/:postId" element={<BlogPost />} />
+
+        <Route path="/login" element={<Login />} />
+
+        {/* Redirect unknown routes */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
